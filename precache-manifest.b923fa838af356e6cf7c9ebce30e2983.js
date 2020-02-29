b0e3dad0f2e7dceabbf222f2715abe39964b7ac5
Melissa Chiang <mchiang62@gmail.com>
2020-02-29 16:06:43 -0500

    Updates

---
+++ precache-manifest.b923fa838af356e6cf7c9ebce30e2983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0338570cb734fc0e0d9cbecd2f5460",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0451663c1df7e25e6310",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f13b673db0b4d218b61b",
+    "url": "/my-portfolio/static/js/2.25136f25.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-portfolio/static/js/2.25136f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0451663c1df7e25e6310",
+    "url": "/my-portfolio/static/js/main.1339e78a.chunk.js"
+  },
+  {
+    "revision": "327500901c72eab58f18",
+    "url": "/my-portfolio/static/js/runtime-main.d4d0ac1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
